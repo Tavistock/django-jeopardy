5f24b3329636b7afb2723800a9cdf567c5ad5da8
tavistock <tavistock91@gmail.com>
2015-07-06 10:46:00 -0400

    heroku app initial commit

---
+++ jeopardy_models/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+class Season(models.Model):
+    title = models.CharField(max_lenth=500)
+    start = models.DateField()
+    end = models.DateField()
+
+class Episode(models.Model):
+    name = models.CharField(max_length=500)
+    game_id = models.IntegerField()
+    title = models.CharField(max_length=500)
+    subtitle = models.CharField(max_length=500)
+    date_aired = models.DateField()
+    season = models.ForeignKey(Season)
+
+class GameRound(models.Model):
+    round_number = models.IntegerField()
+    round_type = models.CharField(max_length=500)
+    episode = models.ForeignKey(Episode)
+
+class Category(models.Model):
+    name = models.CharField(max_length=500)
+    game_round = models.ForeignKey(GameRound)
+
+### catagory (unique for value in catagory)
+class Question(models.Model):
+    value = models.CharField(max_length)
+    question = models.CharField(max_length=1000)
+    answer = models.CharField(max_length=1000)
+    catagory = models.ForeignKey(Category)
