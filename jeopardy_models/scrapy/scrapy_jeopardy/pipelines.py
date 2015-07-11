5e1d50f1e539b239c92c0e642cd158e04e5c2850
Travis McNeill <tavistock91@gmail.com>
2015-07-10 21:38:21 -0400

    added pipeline to save django models using spider

--- jeopardy_models/scrapy/scrapy_jeopardy/pipelines.py
+++ jeopardy_models/scrapy/scrapy_jeopardy/pipelines.py
@@ -5,7 +5,10 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
-from ../../models import Season, Episode, GameRound, Category, Question
+from jeopardy_models.models import Season, Episode, GameRound, Category, Question
+from datetime import datetime
+from django.utils.timezone import make_aware
+from django.db import transaction
 
 class ScrapyJeopardyPipeline(object):
     def process_item(self, item, spider):
@@ -15,15 +18,20 @@ class ScrapyJeopardyPipeline(object):
         #    process_episode(item)
         #elif item['type'] == 'rounds':
         #    process_rounds(item)
+        return item
 
 def process_season(item):
-    season = Season(start=get_date(item['start']),
-                    end=get_date(item['end']),
-                    title = item['title'])
-    season.save()
+    start = get_date(item['start'])
+    end = get_date(item['end'])
+    title = item['title']
+    with transaction.atomic():
+        season = Season(start=start, end=end, title=title)
+        season.save()
 
 def get_date(stamp):
+    stamp_time = None
     try:
-        return time.strptime(stamp, '%Y-%m-%d')
+         stamp_time = datetime.strptime(stamp, '%Y-%m-%d')
     except Exception as e:
-        return time.strptime(stamp, '%Y')
+         stamp_time = datetime.strptime(stamp, '%Y')
+    return make_aware(stamp_time)
